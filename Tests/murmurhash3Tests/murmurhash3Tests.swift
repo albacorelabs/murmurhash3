edee147b35c2070708d05d0d87d02470a0dbf281
fanquake <fanquake@gmail.com>
2019-01-28 16:20:41 +0800

    tests: Add some additional test vectors from Bitcoin Core

--- Tests/murmurhash3Tests/murmurhash3Tests.swift
+++ Tests/murmurhash3Tests/murmurhash3Tests.swift
@@ -7,8 +7,9 @@ final class MurmurHash3Tests: XCTestCase {
         XCTAssertEqual(MurmurHash3.hashString(input, seed), expected)
     }
 
-    // test vectors from https://stackoverflow.com/questions/14747343/murmurhash3-test-vectors
     func testMurmurHash3() {
+
+        // test vectors from https://stackoverflow.com/questions/14747343/murmurhash3-test-vectors
         performTest("", 0, 0)
         performTest("", 1, 0x514E28B7)
         performTest("", 0xffffffff, 0x81F16F39)
@@ -34,6 +35,14 @@ final class MurmurHash3Tests: XCTestCase {
         performTest("abc", 0, 0xB3DD93FA)
 
         performTest("The quick brown fox jumps over the lazy dog", 0x9747B28C, 0x2FA826CD)
+
+        // additional test vectors from Bitcoin Core
+        // https://github.com/bitcoin/bitcoin/blob/master/src/test/hash_tests.cpp#L19
+        performTest("", 0x00000000, 0x00000000)
+        performTest("", 0xFBA4C795, 0x6a396f08) // 0xFBA4C795 is from BIP37
+        performTest("", 0xffffffff, 0x81f16f39)
+        performTest("\0", 0x00000000, 0x514e28b7)
+        performTest("\0", 0xFBA4C795, 0xea3f0b17)
     }
 
     static var allTests = [
